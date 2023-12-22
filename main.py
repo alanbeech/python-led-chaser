ede9f3667dc4a7ea69f3104709d4464f91cf62d4
Alan Beech <alan@beech.me.uk>
2023-12-22 10:05:01 +0000

    implemented chaser

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+# LED Chaser in MicroPython
 
+from machine import Pin
+from time import sleep
 
-# from machine import Pin
-# from time import sleep
-#
-#
-#
-# led = Pin(2, Pin.OUT)
-# while True:
-#     led.value(not led.value())
-#     sleep(.1)
+LED = [25, 33, 32, 27, 14]
+
+del_time = 0.1
+
+for i in range(len(LED)):
+    LED[i] = Pin(LED[i], Pin.OUT)
+
+while True:
+    for i in range(len(LED)):
+        LED[i].value(1)
+        sleep(del_time)
+        LED[i].value(0)
